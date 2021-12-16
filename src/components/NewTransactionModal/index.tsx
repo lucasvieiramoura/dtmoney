99d71a53d3dc250af5d75e57882d61fabef2b651
Lucas Vietos <lucasvieiramoura@gmail.com>
2021-12-16 00:35:35 -0300

    Post transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,8 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 
+import {api} from '../../services/api';
+
 import {Container, TransactionTypeContainer,RadioBox} from './styles';
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -11,11 +13,21 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose} :NewTransactionModalProps) {
-
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
     function handleCreateNewTransaction(event : FormEvent) {
         event.preventDefault()
+       const data = {
+            title,
+            value,
+            category,
+            type
+        };
+
+        api.post('/transactions', data)
     }
 
     return (
@@ -31,8 +43,17 @@ export function NewTransactionModal({isOpen, onRequestClose} :NewTransactionModa
             <Container onSubmit={handleCreateNewTransaction}>
 
                 <h2>Cadastrar Transação</h2>
-                <input placeholder="Título"></input>
-                <input type="number" placeholder="Valor"></input>
+                <input 
+                    placeholder="Título"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}                
+                />
+                <input 
+                    type="number" 
+                    placeholder="Valor"
+                    value={value}
+                    onChange={event => setValue(Number(event.target.value))}                
+                />
                 <TransactionTypeContainer>
                     <RadioBox 
                         type="button"  
@@ -54,7 +75,11 @@ export function NewTransactionModal({isOpen, onRequestClose} :NewTransactionModa
                         <span>Saída</span>
                     </RadioBox>
                 </TransactionTypeContainer>
-                <input placeholder="Categoria"></input>
+                <input 
+                    placeholder="Categoria"
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}                
+                />
                 <button type="submit"> Cadastrar </button>
 
             </Container>
