0d50c9ab0d1d94eca5c7ac0e00ec6eb104349904
Lucas Vietos <lucasvieiramoura@gmail.com>
2021-12-16 22:56:29 -0300

    Seeds API, transactions, setTransactions transaction.map HTML

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createServer, Model } from 'miragejs';
 import {App} from './App';
-import schema from 'miragejs/orm/schema';
 
 createServer({
   models:{
-    transition:Model,
+    transaction: Model,
   },
   
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+          {
+          id:1,
+          title: 'Feelancer de website',
+          type: 'deposit',
+          category:'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-12-16 22:20:00')
+        },
+        {
+          id:2,
+          title: 'Alugel',
+          type: 'withdraw',
+          category:'Casa',
+          amount: 1200,
+          createdAt: new Date('2021-12-10 22:20:00')
+        }
+      ],
+    })
+  },
+
   routes(){
     this.namespace='api';
     this.get('/transactions',() =>{
@@ -17,7 +39,7 @@ createServer({
 
     this.post('/transactions',(schema,request)=>{
       const data = JSON.parse(request.requestBody)
-      return schema.create('transcation',data)
+      return schema.create('transaction',data)
       });
   }
 }
